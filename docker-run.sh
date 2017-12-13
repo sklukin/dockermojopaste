82c9a09ab043d592c97919344ba4cfb1ba3ad478
sklukin <pythonsmail@gmail.com>
2017-12-13 10:45:17 +0500

    Start by hypnotoad

--- docker-run.sh
+++ docker-run.sh
@@ -12,6 +12,9 @@ CMD="$CMD --name mojopaste"
 CMD="$CMD -v $PWD:/usr/src/app"
 CMD="$CMD -p 5555:8080"
 CMD="$CMD mojopaste_v1"
-CMD="$CMD sh -c 'cd /usr/src/app && exec mojopaste daemon --listen http://*:8080'"
+# NOTE: Start by deamon
+# CMD="$CMD sh -c 'cd /usr/src/app && exec mojopaste daemon --listen http://*:8080'"
+# NOTE start by hypnotoad
+CMD="$CMD sh -c 'cd /usr/src/app && exec hypnotoad -f $(which mojopaste)'"
 
 eval $CMD
